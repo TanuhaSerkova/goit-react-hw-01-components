2e388ea39cb989bbe8cd3496945ccb6ebf2315a8
TanuhaSerkova <89786945+TanuhaSerkova@users.noreply.github.com>
2022-05-03 15:41:51 +0300

    TransactionHistory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import user from 'components/SocialProfile/user.json';
 import data from 'components/Statistics/data.json';
 import friends from 'components/FriendList/friends.json';
+import transactions from 'components/TransactionHistory/transactions.json';
 import { Profile } from 'components/SocialProfile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
-
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import { Container } from './App.styled.jsx';
-
 
 
 export function App() {
@@ -21,6 +21,7 @@ return (
         />
         <Statistics title="Upload stats" stats={data} />
         <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
     </Container>
 );
 }
